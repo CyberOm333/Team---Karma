33291d6190d0dda3af692096cee3945ae35cf7de
OM <cyber.x.code333@gmail.com>
2022-11-17 16:00:01 +0530

    Day 2 Complete

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int days = 3;
+    final String name = "Om Chaudhari";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To Flutter $days ! by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
